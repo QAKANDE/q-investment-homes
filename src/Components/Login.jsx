2e11ea79995fa38e84db11f18c65dc863f3fe53b
QOA <58850207+QAKANDE@users.noreply.github.com>
2020-11-19 11:57:39 +0000

    chnaged back to token

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -43,7 +43,7 @@ class Login extends Component {
       });
       if (response.ok) {
         const token = await response.json();
-        localStorage["accessToken"] = token.token ;
+        localStorage["accessToken"] = token ;
       localStorage["email"] = this.state.loginDetails.email;
       if (localStorage.accessToken) {
         const authorize = await fetch(
