0ce9836bff32dcd571c6c405d99d55f42a0be5f1
QOA <58850207+QAKANDE@users.noreply.github.com>
2020-11-19 23:13:12 +0000

    heroku bitch

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -29,7 +29,7 @@ class NavBar extends Component {
           }
         );
         const userDetails = await response.json();
-        console.log(userDetails)
+        console.log("navBar usedetails", userDetails)
   
         this.setState({
           userFirstName: userDetails.firstName,
